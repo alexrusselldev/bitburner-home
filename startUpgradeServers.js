90002fde9e5c4c72c29293624b009f30d4fd8153
Alex Russell <alexrusselldev@gmail.com>
2024-03-29 14:44:40 +0000

    fix: 🐛 add host and increase wait in start upgrades script

--- startUpgradeServers.js
+++ startUpgradeServers.js
@@ -3,13 +3,13 @@ export async function main(ns) {
   let ram = 256
 
   while (true) {
-    ns.exec('remote/upgradeAndHack.js', host, 1)
+    ns.exec('remote/upgradeAndHack.js', 'home', 1)
 
     if (ram == 1048576) {
       break
     }
 
-    await ns.sleep(500)
+    await ns.sleep(10000)
 
     ram = ram * 2
   }
