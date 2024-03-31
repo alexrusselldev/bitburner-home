2b3108b91fae678a2f6266cd8f32038db5497110
Alex Russell <alexrusselldev@gmail.com>
2024-03-31 21:01:33 +0100

    feat: ✨ add autocomplete to upgrade and hack script

--- remote/upgradeAndHack.js
+++ remote/upgradeAndHack.js
@@ -1,6 +1,18 @@
 import { startHacks } from 'lib/startHacks'
 
-/** @param {NS} ns */
+export function autocomplete(_data, args) {
+  if (args.length == 0) {
+    return [
+      2, 4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
+      65536, 131072, 262144, 524288, 1048576,
+    ]
+  }
+  return []
+}
+
+/**
+ * @param {NS} ns
+ */
 export async function main(ns) {
   const ram = ns.args[0]
 
