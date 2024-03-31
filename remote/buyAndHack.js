cf17cf01a1eeeff3692e9e548626523cdcdf1a8b
Alex Russell <alexrusselldev@gmail.com>
2024-03-31 18:09:50 +0100

    fix: 🐛 fix autocomplete for buyandhack

--- remote/buyAndHack.js
+++ remote/buyAndHack.js
@@ -1,10 +1,13 @@
 import { startHacks } from 'lib/startHacks'
 
-export function autocomplete() {
-  return [
-    2, 4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
-    65536, 131072, 262144, 524288, 1048576,
-  ]
+export function autocomplete(_data, args) {
+  if (args.length == 0) {
+    return [
+      2, 4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
+      65536, 131072, 262144, 524288, 1048576,
+    ]
+  }
+  return []
 }
 
 /** @param {NS} ns */
