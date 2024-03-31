0f09fb63379574fb6c3fd1cbf247814ebcb85d58
Alex Russell <alexrusselldev@gmail.com>
2024-03-31 17:40:05 +0100

    feat: ✨ add hackserver autocomplete

--- hackServer.js
+++ hackServer.js
@@ -1,26 +1,30 @@
+export function autocomplete(data) {
+  return [...data.servers]
+}
+
 /** @param {NS} ns */
 export async function main(ns) {
-  const server = ns.args[0]
+  for (const server of ns.args) {
+    if (ns.fileExists('BruteSSH.exe')) {
+      ns.brutessh(server)
+    }
+    if (ns.fileExists('FTPCrack.exe')) {
+      ns.ftpcrack(server)
+    }
+    if (ns.fileExists('HTTPWorm.exe')) {
+      ns.httpworm(server)
+    }
+    if (ns.fileExists('SQLInject.exe')) {
+      ns.sqlinject(server)
+    }
+    if (ns.fileExists('relaySMTP.exe')) {
+      ns.relaysmtp(server)
+    }
 
-  if (ns.fileExists('BruteSSH.exe')) {
-    ns.brutessh(server)
-  }
-  if (ns.fileExists('FTPCrack.exe')) {
-    ns.ftpcrack(server)
-  }
-  if (ns.fileExists('HTTPWorm.exe')) {
-    ns.httpworm(server)
-  }
-  if (ns.fileExists('SQLInject.exe')) {
-    ns.sqlinject(server)
-  }
-  if (ns.fileExists('relaySMTP.exe')) {
-    ns.relaysmtp(server)
-  }
+    ns.nuke(server)
 
-  ns.nuke(server)
+    const servers = ns.read('servers.txt')
 
-  const servers = ns.read('servers.txt')
-
-  ns.write('servers.txt', `\n${server}`)
+    ns.write('servers.txt', `\n${server}`)
+  }
 }
