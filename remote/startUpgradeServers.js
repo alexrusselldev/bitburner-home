f476c4ab668bca6294e29d327a6aa90d2990398a
Alex Russell <alexrusselldev@gmail.com>
2024-04-01 13:57:45 +0100

    feat: ✨ add logging to start upgrade servers script

--- remote/startUpgradeServers.js
+++ remote/startUpgradeServers.js
@@ -10,8 +10,6 @@ export function autocomplete(_data, args) {
 
 /**
  * @param {NS} ns
- * TODO: Add terminal logging
- * TODO: Reduce timeout
  */
 export async function main(ns) {
   ns.disableLog('ALL')
@@ -30,13 +28,35 @@ export async function main(ns) {
   }
 
   while (true) {
-    ns.exec('remote/upgradeAndHack.js', 'home', 1, ram)
-
-    if (ram == 1048576) {
+    if (availableRam < ns.getScriptRam('remote/upgradeAndHack.js')) {
+      ns.tprint(
+        `WARN: Upgrade scripts started up to ${
+          ram / 2
+        }Gb. Out of memory. Exiting...`
+      )
       break
     }
 
-    await ns.sleep(10000)
+    ns.print(`Started upgrade script for ${ram}Gb`)
+    ns.exec('remote/upgradeAndHack.js', 'home', 1, ram)
+
+    if (ram == 1048576) {
+      ns.tprint(`SUCCESS: Upgrade scripts started to max RAM value`)
+      break
+    }
+
+    const availableRam =
+      ns.getServerMaxRam(ns.getHostname()) -
+      ns.getServerUsedRam(ns.getHostname())
+
+    if (availableRam < ns.getScriptRam('remote/upgradeAndHack.js')) {
+      ns.tprint(
+        `WARN: Upgrade scripts started up to ${ram}Gb. Out of memory. Exiting...`
+      )
+      break
+    }
+
+    await ns.sleep(500)
 
     ram = ram * 2
   }
