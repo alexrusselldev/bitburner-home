d86fae4404e7500fd740e249d9c0e811a4575551
Alex Russell <alexrusselldev@gmail.com>
2024-04-01 16:20:11 +0100

    feat: ✨ add logic for separating honeypots from servers

--- rootServer.js
+++ rootServer.js
@@ -8,6 +8,14 @@ export function autocomplete(data) {
  * @param {NS} ns
  */
 export async function main(ns) {
+  const serverFile = ns.read('servers.txt').split('\n')
+  const servers =
+    serverFile.length == 1 && serverFile[0] == [''] ? [] : serverFile
+
+  const honeypotsFile = ns.read('honeypots.txt').split('\n')
+  const honeypots =
+    honeypotsFile.length == 1 && honeypotsFile[0] == [''] ? [] : honeypotsFile
+
   for (const server of ns.args) {
     if (!ns.serverExists(server)) {
       ns.tprint(`WARN: Could not connect to server ${server}. Skipping...`)
@@ -34,13 +42,31 @@ export async function main(ns) {
 
     ns.nuke(server)
     if (ns.getServerMoneyAvailable(server) == 0) {
+      if (honeypots.includes(server)) {
+        ns.tprint(
+          `SUCCESS: ${server} rooted. $0 available. ${server} already exists in honeypots.txt. Skipping write.`
+        )
+        honeypots.push(server)
+        continue
+      }
+
       ns.tprint(
-        `SUCCESS: ${server} rooted. $0 available. Skipping write to servers.txt`
+        `SUCCESS: ${server} rooted. $0 available. Writing to honeypots.txt`
+      )
+
+      ns.write('honeypots.txt', `\n${server}`, 'a')
+      continue
+    }
+
+    if (servers.includes(server)) {
+      ns.tprint(
+        `SUCCESS: ${server} rooted. ${server} already exists in servers.txt. Skipping write.`
       )
       continue
     }
 
     ns.write('servers.txt', `\n${server}`, 'a')
+    servers.push(server)
     ns.tprint(`SUCCESS: ${server} rooted and added to servers.txt`)
   }
 }
